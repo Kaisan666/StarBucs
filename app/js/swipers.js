5b9274d85ca79b6c5708ca04300bc60160f4d37c
Sasha <sasha41187@gmail.com>
2025-06-09 15:36:06 +0300

    swiper

--- app/js/swipers.js
+++ app/js/swipers.js
@@ -4,12 +4,22 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
-
-
-const delicious = document.querySelector(".new-products")
-if (delicious){
-    const deliciousSwiper = new Swiper(delicious.querySelector(".swiper"), {
-        slidesPerView : "auto",
-        spaceBetween : 54,
-    })
+const delicious = document.querySelector('.new-products');
+if (delicious) {
+  const deliciousSwiper = new Swiper(delicious.querySelector('.swiper'), {
+    slidesPerView: 'auto',
+    spaceBetween: 54,
+  });
+}
+const events = document.querySelector('.events');
+if (events) {
+  const eventsSwiper = new Swiper(events.querySelector('.swiper'), {
+    slidesPerView: "auto",
+    // slidesPerView: 2,
+    spaceBetween: 30,
+    grid: {
+        rows: 2,
+        fill: 'row',
+      },
+  });
 }
